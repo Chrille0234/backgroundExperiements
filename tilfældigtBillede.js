295b07dc07e0c26f431092fe149cb2ec7d256ad9
Chrille0234 <madsen.chr@hotmail.com>
2022-10-31 17:22:45 +0100

    made the background button actually work

--- tilfældigtBillede.js
+++ tilfældigtBillede.js
@@ -1,7 +1,36 @@
 function billede(){
     var test = (Math.random() * 10) + 1;
     
-    if (test == 1){
-        
+    
+    if (parseInt(test) == 1){
+        document.getElementById("bd").style.backgroundImage="url(billeder/testphoto.png)"
     }
+    else if (parseInt(test) == 2){
+        document.getElementById("bd").style.backgroundImage="url(billeder/testphoto.png)"
+    }
+    else if (parseInt(test) == 3){
+        document.getElementById("bd").style.backgroundImage="url(billeder/testphoto.png)"
+    }
+    else if (parseInt(test) == 4){
+        document.getElementById("bd").style.backgroundImage="url(billeder/testphoto.png)"
+    }
+    else if (parseInt(test) == 5){
+        document.getElementById("bd").style.backgroundImage="url(billeder/testphoto.png)"
+    }
+    else if (parseInt(test) == 6){
+        document.getElementById("bd").style.backgroundImage="url(billeder/testphoto.png)"
+    }
+    else if (parseInt(test) == 7){
+        document.getElementById("bd").style.backgroundImage="url(billeder/testphoto.png)"
+    }
+    else if (parseInt(test) == 8){
+        document.getElementById("bd").style.backgroundImage="url(billeder/testphoto.png)"
+    }
+    else if (parseInt(test) == 9){
+        document.getElementById("bd").style.backgroundImage="url(billeder/testphoto.png)"
+    }
+    else if (parseInt(test) == 10){
+        document.getElementById("bd").style.backgroundImage="url(billeder/testphoto.png)"
+    }
+
 }
