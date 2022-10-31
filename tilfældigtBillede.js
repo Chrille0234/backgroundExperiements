3ad45794ec8b46701ef09a022dc1281a1e3404e7
Chrille0234 <113505641+Chrille0234@users.noreply.github.com>
2022-10-31 17:25:18 +0100

    Update tilfældigtBillede.js

--- tilfældigtBillede.js
+++ tilfældigtBillede.js
@@ -3,19 +3,19 @@ function billede(){
     
     
     if (parseInt(test) == 1){
-        document.getElementById("bd").style.backgroundImage="url(billeder/testphoto.png)"
+        document.getElementById("bd").style.backgroundImage="url(https://play-lh.googleusercontent.com/V_P-I-UENK93ahkQgOWel8X8yFxjhOOfMAZjxXrqp311Gm_RBtlDXHLQhwFZN8n4aIQ)"
     }
     else if (parseInt(test) == 2){
-        document.getElementById("bd").style.backgroundImage="url(billeder/testphoto.png)"
+        document.getElementById("bd").style.backgroundImage="url(https://play-lh.googleusercontent.com/V_P-I-UENK93ahkQgOWel8X8yFxjhOOfMAZjxXrqp311Gm_RBtlDXHLQhwFZN8n4aIQ)"
     }
     else if (parseInt(test) == 3){
-        document.getElementById("bd").style.backgroundImage="url(billeder/testphoto.png)"
+        document.getElementById("bd").style.backgroundImage="url(https://play-lh.googleusercontent.com/V_P-I-UENK93ahkQgOWel8X8yFxjhOOfMAZjxXrqp311Gm_RBtlDXHLQhwFZN8n4aIQ)"
     }
     else if (parseInt(test) == 4){
-        document.getElementById("bd").style.backgroundImage="url(billeder/testphoto.png)"
+        document.getElementById("bd").style.backgroundImage="url(https://play-lh.googleusercontent.com/V_P-I-UENK93ahkQgOWel8X8yFxjhOOfMAZjxXrqp311Gm_RBtlDXHLQhwFZN8n4aIQ)"
     }
     else if (parseInt(test) == 5){
-        document.getElementById("bd").style.backgroundImage="url(billeder/testphoto.png)"
+        document.getElementById("bd").style.backgroundImage="url(https://play-lh.googleusercontent.com/V_P-I-UENK93ahkQgOWel8X8yFxjhOOfMAZjxXrqp311Gm_RBtlDXHLQhwFZN8n4aIQ)"
     }
     else if (parseInt(test) == 6){
         document.getElementById("bd").style.backgroundImage="url(billeder/testphoto.png)"
